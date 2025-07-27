82b7dd3922eebc0b738cf55a8399db323fd17821
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-27 05:32:46 -0300

    add ft_printf_utils.c with utility functions for formatted output

---
+++ ft_printf_utils.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/27 05:28:14 by lmelo-do          #+#    #+#             */
+/*   Updated: 2025/07/27 05:32:17 by lmelo-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	return write(1, &c, 1);
+}
+
+int	ft_putstr(const char *s)
+{
+	int	count;
+
+	if (!s)
+		return (ft_putstr("(null)"));
+	count = 0;
+	while (*s)
+		count += ft_putchar(*s++);
+	return (count);
+}
+
+int	ft_putnbr(int n)
+{
+	unsigned int	nb;
+	int				count;
+
+	count = 0;
+	if (n < 0)
+	{
+		count += ft_putchar('-');
+		nb = (unsigned int)(-n);
+	}
+	else
+		nb = (unsigned int)n;
+	if (nb >= 10)
+		count += ft_putnbr(nb / 10);
+	count += ft_putchar((nb % 10) + '0');
+	return (count);
+}
+
+int	ft_putnbr_unsigned(unsigned int n)
+{
+	int	count = 0;
+
+	if (n >= 10)
+		count += ft_putnbr_unsigned(n / 10);
+	count += ft_putchar((n % 10) + '0');
+	return (count);
+}
+
+int	ft_puthex(unsigned int n, int uppercase)
+{
+	char	*base;
+	int		count = 0;
+
+	if (uppercase)
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+	if (n >= 16)
+		count += ft_puthex(n / 16, uppercase);
+	count += ft_putchar(base[n % 16]);
+	return (count);
+}
+
+static int	ft_puthex_long(unsigned long n)
+{
+	char	*base;
+	int		count = 0;
+
+	base = "0123456789abcdef";
+	if (n >= 16)
+		count += ft_puthex_long(n / 16);
+	count += ft_putchar(base[n % 16]);
+	return (count);
+}
+
+int	ft_putptr(void *ptr)
+{
+	int count;
+
+	count = 0;
+	count += ft_putstr("0x");
+	count += ft_puthex_long(ptr);
+	return (count);
+}
