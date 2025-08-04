cf5e8a8a95f72e00f6d92c1081185de2af1ad58f
lmelo-do <lmelo-do@student.42sp.org.br>
2025-08-04 17:21:23 -0300

    fix: update header comment and refactor handle_format for clarity

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 04:35:08 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/07/27 05:02:44 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/08/04 17:17:14 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,19 @@ static int	handle_format(char c, va_list args)
 {
 	if (c == 'c')
 		return ft_putchar(va_arg(args, int));
-	else if (c == 's')
+	if (c == 's')
 		return ft_putstr(va_arg(args, char *));
-	else if (c == 'p')
-		return ft_putptr((unsigned long)va_arg(args, void *));
-	else if (c == 'd' || c == 'i')
+	if (c == 'p')
+		return ft_putptr(va_arg(args, void *));
+	if (c == 'd' || c == 'i')
 		return ft_putnbr(va_arg(args, int));
-	else if (c == 'u')
+	if (c == 'u')
 		return ft_putnbr_unsigned(va_arg(args, unsigned int));
-	else if (c == 'x')
+	if (c == 'x')
 		return ft_puthex(va_arg(args, unsigned int), 0);
-	else if (c == 'X')
+	if (c == 'X')
 		return ft_puthex(va_arg(args, unsigned int), 1);
-	else if (c == '%')
+	if (c == '%')
 		return ft_putchar('%');
 	return (0);
 }
