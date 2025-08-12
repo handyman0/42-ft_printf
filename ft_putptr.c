e15618b79522503dc84b4ae0076c0ee336c41a8e
lmelo-do <lmelo-do@student.42sp.org.br>
2025-08-11 22:24:40 -0300

    fix: removed return unsed and removed count eather

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 05:28:14 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/08/07 19:59:05 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/08/11 22:18:18 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 int	ft_putptr(void *ptr)
 {
-	int	count;
-
 	if (ptr == NULL)
 		return (ft_putstr("(nil)"));
 	return (ft_putstr("0x") + ft_puthex((unsigned long long)ptr, 0));
-	return (count);
 }
